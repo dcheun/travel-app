563ff706d37d6c7bb332cb2bad25484d34b4b500
Danny Cheun <dcheun@gmail.com>
2020-10-15 23:44:20 -0700

    feat: Add grid layout
    Add flexible grid for better layout of the page.

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,9 @@ import { checkForLocation } from "./js/locationChecker";
 import { checkForDate } from "./js/dateChecker";
 
 import "./styles/styles.scss";
+import "./styles/grid.scss";
+import "./styles/header.scss";
+import "./styles/footer.scss";
 
 // Attach listener to generate.
 document.getElementById("generate").addEventListener("click", handleSubmit);
