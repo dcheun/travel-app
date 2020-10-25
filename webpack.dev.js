3b29bdcecd83fa753d5eda2217fc68264447cc22
Danny Cheun <dcheun@gmail.com>
2020-10-25 12:58:13 -0700

    feat: Add service workers back to dev
    Add Server Workers back into dev environment as a last step.

--- webpack.dev.js
+++ webpack.dev.js
@@ -37,6 +37,6 @@ module.exports = {
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false,
     }),
-    // new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
